4be9f3c7d4f0b59b4e5dd5d9c2fc175c8636694d
FSRR <sebastianretizmail.com>
2020-12-29 22:01:05 -0500

    Terminado

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
-const card_number = Array.from(document.querySelectorAll('.number'))
+const cards = document.querySelectorAll('.number')
+const frontCard = Array.from(document.querySelectorAll('.front-number'))
+const backCard = Array.from(document.querySelectorAll('.back-number'))
+
 const date = new Date('01/01/2021 12:00 AM')
+let difference
 let timer
 
 const seconds = 1000
@@ -7,55 +11,115 @@ const minutes = seconds * 60
 const hours = minutes * 60
 const days = hours * 24
 
+let counterSeconds = 0
+let counterMinutes = 0
+let counterHours = 0
+let counterDays = 0
+
+let textCardSeconds
+let textCardMinutes
+let textCardHours
+let textCardDays
+
+
+const showSeconds = () => {
+    // console.log('seconds');
+    if(counterSeconds % 2 == 0) {
+        backCard[3].textContent = `${Math.floor(difference % minutes / seconds)}`.padStart(2,0)
+    
+        cards[3].classList.toggle('flip-card')
+        textCardSeconds = backCard[3].textContent
+    } else {
+        frontCard[3].textContent = `${Math.floor(difference % minutes / seconds)}`.padStart(2,0)
+    
+        cards[3].classList.toggle('flip-card')
+        textCardSeconds = frontCard[3].textContent
+    }
+    counterSeconds++
+}
+
+const showMinutes = () => {
+    // console.log('seconds');
+    if(counterMinutes % 2 == 0) {
+        backCard[2].textContent = `${Math.floor((difference % hours) / minutes)}`.padStart(2,0)
+        
+        cards[2].classList.toggle('flip-card')
+        textCardMinutes = backCard[2].textContent
+    } else {
+        frontCard[2].textContent = `${Math.floor((difference % hours) / minutes)}`.padStart(2,0)
+        
+        cards[2].classList.toggle('flip-card')
+        textCardMinutes = frontCard[2].textContent
+    }
+    counterMinutes++
+}
+
+const showHours = () => {
+    // console.log('seconds');
+    if(counterHours % 2 == 0) {
+        backCard[1].textContent = `${Math.floor((difference % days) / hours)}`.padStart(2,0)
+        
+        cards[1].classList.toggle('flip-card')
+        textCardHours = backCard[1].textContent
+    } else {
+        frontCard[1].textContent = `${Math.floor((difference % days) / hours)}`.padStart(2,0)
+    
+        cards[1].classList.toggle('flip-card')
+        textCardHours = frontCard[1].textContent
+    }
+    counterHours++
+}
+
+const showDays = () => {
+    // console.log('seconds');
+    if(counterDays % 2 == 0) {
+        backCard[0].textContent = `${Math.floor(difference / days)}`.padStart(2,0)
+    
+        cards[0].classList.toggle('flip-card')
+        textCardDays = backCard[0].textContent
+    } else {
+        frontCard[0].textContent = `${Math.floor(difference / days)}`.padStart(2,0)
+    
+        cards[0].classList.toggle('flip-card')
+        textCardDays = frontCard[0].textContent
+    }
+    counterDays++
+}
+
 
 const count = (time = 'all') => {
     const now = new Date
-
-    const difference = date - now
+    
+    difference = date - now
 
     if(difference < 0) {
         console.log('terminooooo');
         clearInterval(timer)
     } else {
+        // cards[0].classList.remove()
         switch (time) {
             case 'seconds':
                 console.log('seconds');
-                card_number[3].classList.toggle('flip-card')
-                
-                card_number[3].textContent = `${Math.floor(difference % minutes / seconds)}`.padStart(2,0)
+                showSeconds()
                 break;
                 
             case 'minutes':
                 console.log('minutes');
-                card_number[3].textContent = `${Math.floor(difference % minutes / seconds)}`.padStart(2,0)
-                card_number[2].textContent = `${Math.floor((difference % hours) / minutes)}`.padStart(2,0)
-
-                card_number[3].classList.toggle('flip-card')
-                card_number[2].classList.toggle('flip-card')
+                showSeconds()
+                showMinutes()
                 break;
                 
             case 'hours':
-                console.log('hours');
-                card_number[3].textContent = `${Math.floor(difference % minutes / seconds)}`.padStart(2,0)
-                card_number[2].textContent = `${Math.floor((difference % hours) / minutes)}`.padStart(2,0)
-                card_number[1].textContent = `${Math.floor((difference % days) / hours)}`.padStart(2,0)
-
-                card_number[3].classList.toggle('flip-card')
-                card_number[2].classList.toggle('flip-card')
-                card_number[1].classList.toggle('flip-card')
+                showSeconds()                
+                showMinutes()
+                showHours()
                 break;
                 
             default:
-                console.log('default');
-                card_number[3].textContent = `${Math.floor(difference % minutes / seconds)}`.padStart(2,0)
-                card_number[2].textContent = `${Math.floor((difference % hours) / minutes)}`.padStart(2,0)
-                card_number[1].textContent = `${Math.floor((difference % days) / hours)}`.padStart(2,0)
-                card_number[0].textContent = `${Math.floor(difference / days)}`.padStart(2,0)
-
-                card_number[3].classList.toggle('flip-card')
-                card_number[2].classList.toggle('flip-card')
-                card_number[1].classList.toggle('flip-card')
-                card_number[0].classList.toggle('flip-card')
+                showSeconds()
+                showMinutes()
+                showHours()
+                showDays()
                 break;
         }
     }
@@ -64,12 +128,9 @@ const count = (time = 'all') => {
 onload = count()
 
 const countDown = () => {
-    if(card_number[1].textContent == 00 && card_number[2].textContent == 00 &&
-        card_number[3].textContent == 00) {
-        count()
-    } else if(card_number[2].textContent == 00 && card_number[3].textContent == 00) {
-        count('hours')
-    } else if(card_number[3].textContent == 00) count('minutes')
+    if(textCardSeconds == 00 && textCardMinutes == 00 &&textCardHours == 00) count()
+    else if(textCardSeconds == 00 && textCardMinutes == 00) count('hours')
+    else if(textCardSeconds == 00) count('minutes')
     else count('seconds')
 }
 
